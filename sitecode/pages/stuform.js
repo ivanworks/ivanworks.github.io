f386651774065b457f63037618c81f886c6ef4cd
RitaliJ <ritaliaarav@gmail.com>
2021-12-28 12:21:41 -0800

    Added js file for sign up forms

---
+++ sitecode/pages/stuform.js
@@ -0,0 +1,19 @@
+
+
+const form = document.querySelector("form");
+
+/* inputs */
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const hs = document.querySelector("#hs");
+const grade = document.querySelector("#grade");
+const pathway = document.querySelector("#pathway");
+const numClasses = document.querySelector("#numClasses");
+const why = document.querySelector("#why");
+const teacher = document.querySelector("#teacher");
+const teacherEmail = document.querySelector("#teacherEmail");
+
+/* event listener */
+form.addEventListener('submit', function(e){
+	
+});
