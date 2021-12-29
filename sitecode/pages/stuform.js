d570dd85866399bef7a2115618b3ac90be0143d8
RitaliJ <ritaliaarav@gmail.com>
2021-12-28 23:22:13 -0800

    Finished backend for student form data and connected to spreadsheet using fetch

--- sitecode/pages/stuform.js
+++ sitecode/pages/stuform.js
@@ -1,5 +1,6 @@
 
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyHPH5OTZxAcGkbqn--Jyr-hJQ03z138gxa3LBBTCb5hd4fFNUfHqtpYTa-5zcCdMGa/exec'
 const form = document.querySelector("form");
 
 /* inputs */
@@ -13,7 +14,24 @@ const why = document.querySelector("#why");
 const teacher = document.querySelector("#teacher");
 const teacherEmail = document.querySelector("#teacherEmail");
 
+
 /* event listener */
 form.addEventListener('submit', function(e){
-	
+	e.preventDefault()
+	fetch(scriptURL, { method: 'POST', body:new FormData(form)})
+		.then(response => (response.json()))
+        .then(response => {
+        	name.value = '';
+        	email.value = '';
+        	hs.value = '';
+        	grade.value = '';
+        	pathway.value = '';
+        	numClasses.value = '';
+        	why.value = '';
+        	teacher.value = '';
+        	teacherEmail.value = '';
+        	alert("Application Successfully Submitted! We will contact you soon.");
+    	})
+        .catch(error => alert('Error!', error.message))
 });
+
