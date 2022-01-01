c80479ccddc781d4df84f847ef55d93ba87107ff
RitaliJ <ritaliaarav@gmail.com>
2021-12-31 19:10:30 -0800

    Combined all sections into one home page and fixed styles

---
+++ ritali_folder/sitecode/pages/empform.js
@@ -0,0 +1,29 @@
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzjG0EBs2w6U_a_Lt9gz1ymPo4WWJX5Btnh8-QURMxhDMYnGNAfpCgTLCA31Yx1tmlsyw/exec'
+const form = document.querySelector("form");
+
+/* inputs */
+const name = document.querySelector("#employerName");
+const email = document.querySelector("#employerEmail");
+const company = document.querySelector("#companyName");
+const why = document.querySelector("#why");
+const numInterns = document.querySelector("#numInterns");
+
+
+/* event listener */
+form.addEventListener('submit', function(e){
+	e.preventDefault()
+	fetch(scriptURL, { method: 'POST', body:new FormData(form)})
+		.then(response => (response.json()))
+        .then(response => {
+        	name.value = '';
+        	email.value = '';
+        	company.value = '';
+        	why.value = '';
+        	numInterns.value = '';
+        	alert("Application Successfully Submitted! We will contact you soon.");
+    	})
+        .catch(error => alert('Error!', error.message))
+});
+
