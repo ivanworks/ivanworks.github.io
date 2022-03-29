deef99b6f7d3f5c7c0f96b42a21bd0ff6fe9e172
RitaliJ <ritaliaarav@gmail.com>
2022-03-28 23:18:25 -0700

    Connected team cards to modals for functionality

--- sitecode/SJWorksTeam.js
+++ sitecode/SJWorksTeam.js
@@ -1,19 +1,21 @@
 
 /* Modal Functionality */
+
 var exampleModal = document.getElementById('teamModal')
 
 exampleModal.addEventListener('show.bs.modal', function (event) {
   // Button that triggered the modal
-  var button = event.relatedTarget;
+  var card = event.relatedTarget;
   // Extract info from data-bs-* attributes
-  var person = button.getAttribute('data-bs-whatever');
+  var person = card.getAttribute('data-bs-whatever');
+  console.log(person)
   // If necessary, you could initiate an AJAX request here
   // and then do the updating in a callback.
   //
   // Update the modal's content.
   var modalTitle = exampleModal.querySelector('.modal-title');
-  var modalBodyInput = exampleModal.querySelector('modal-description');
+  var modalBodyDescription = exampleModal.querySelector('modal-description');
 
-  modalTitle.textContent = person
-  modalBodyInput.value = person
+  modalTitle.textContent = person;
+  modalBodyDescription.innerText = person;
 })
