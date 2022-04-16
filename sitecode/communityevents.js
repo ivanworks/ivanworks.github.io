642ee2d96a2504e28c0f6640f844123c8517ef02
RitaliJ <ritaliaarav@gmail.com>
2022-04-16 00:39:04 -0700

    Connected modal functionality for events

--- sitecode/communityevents.js
+++ sitecode/communityevents.js
@@ -2,6 +2,18 @@
 /* Input Image Links with the format: https://drive.google.com/uc?export=view&id=XXX
 XXX is the id extracted from the image that was uploaded to google drive */
 
+// Make a list for each attribute to store the data
+var titles = [];
+var images = [];
+var dates = [];
+var locations = [];
+// Access modal
+var exampleModal = document.getElementById('eventModal');
+var modalTitle = exampleModal.querySelector('.modal-title');
+var modalDate = exampleModal.querySelector('.modal-date');
+var modalLocation = exampleModal.querySelector('.modal-location');
+
+
 // AJAX call
 const request = new XMLHttpRequest();
 request.open('GET', 'events.json');
@@ -16,12 +28,6 @@ request.addEventListener('load', function(e){
   var image;
   var date;
   var location;
-
-  // Make a list for each attribute to store the data
-  var titles = [];
-  var images = [];
-  var dates = [];
-  var locations = [];
 
   for (var i=1; i<=4; i++){
     console.log(i)
@@ -47,28 +53,30 @@ request.addEventListener('load', function(e){
 
   }
   return false;
+  
 });
 
 
-
 /* Modal Functionality */
+  exampleModal.addEventListener('show.bs.modal', function (e) {
+    if((titles?.length) && (dates?.length) && (locations?.length)){
+      // condition tests if the events name list has elements
+      // Button that triggered the modal
+      var card = e.relatedTarget;
+      // Extract info from data-bs-* attributes
+      var eventNum = parseInt(card.getAttribute('data-bs-whatever'));
 
-var exampleModal = document.getElementById('eventModal')
+      // Select the modal's content
+      modalTitle.textContent = titles[eventNum - 1];
+      modalDate.textContent = "Date: " + dates[eventNum - 1];
+      modalLocation.textContent = "Location: " + locations[eventNum - 1];
+      console.log(titles[eventNum - 1]);
+      console.log(dates[eventNum - 1]);
+      console.log(locations[eventNum - 1]);
 
-exampleModal.addEventListener('show.bs.modal', function (e) {
-  // Button that triggered the modal
-  var card = e.relatedTarget;
-  // Extract info from data-bs-* attributes
-  var eventNum = parseInt(card.getAttribute('data-bs-whatever'));
+    }
+  })
 
-  // Select the modal's content
-  var modalTitle = exampleModal.querySelector('.modal-title');
-  var modalDate = exampleModal.querySelector('modal-date');
-  var modalLocation = exampleModal.querySelector('modal-location');
 
-  modalTitle.textContent = titles[eventNum - 1];
-  modalDate.innerText = dates[eventNum - 1];
-  modalLocation.innerText = locations[eventNum - 1];
-})
 
 
